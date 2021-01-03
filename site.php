4ba5d09238e02df0179640213d5e51682cdd073d
yoorhim <choi.yoorhim@gmail.com>
2021-01-02 22:30:44 -0500

    Added share array max length

--- site.php
+++ site.php
@@ -29,13 +29,22 @@
       if (isset($get_shares)) {
         //push user input into front of array
         array_unshift($get_shares, $_POST["share"]); 
+        //remove last value of array when reaches max length
+        $max_share = 30;
+        if (sizeof($get_shares) > $max_share) {
+          array_pop($get_shares);
+        }
         //convert array to string
         echo implode(" | ", $get_shares);
         //save updated array into file
         $save_shares = file_put_contents("saved_shares.txt", serialize($get_shares));
 
       }
-      
+  /*I think this isn't recognizing the "share" variable until a user input is provided?
+        I get this error message when I first load the page:
+        "Warning: Undefined array key "share" in C:\Users\mihro\www\site.php on line 31"
+        But that disappears after I provide an input.
+      */    
   ?>
 
   </body>
